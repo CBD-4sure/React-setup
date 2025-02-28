2e560884c604fda1bf47d3db76064e18011ce06f
CBD-4sure <cbdhangar08@gmail.com>
2025-02-28 23:26:47 +0530

    changed package details

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const heading2 = React.createElement("h2", {}, "Hi everyone");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render([heading1,heading2]);
 
-console.log("hi everyone");
+console.log("hi everyone again");
