888714b369240dda87df55d614e6b95df0b698a9
CBD-4sure <cbdhangar08@gmail.com>
2025-02-28 19:26:53 +0530

    created hello world using react

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1", {}, "Hi all");
+const heading2 = React.createElement("h2", {}, "Hi everyone");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render([heading1,heading2]);
+
 console.log("hi everyone");
