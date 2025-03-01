11fb7aa5a1e5e949c8a262cfeb9875d79a77f276
CBD-4sure <cbdhangar08@gmail.com>
2025-03-01 14:34:37 +0530

    created functional components

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement("h1", {}, "Hi all");
-const heading2 = React.createElement("h2", {}, "Hi everyone");
+const Heading2 = () => <h2>h2 here</h2>;
+const Heading1 = () => (
+	<>
+		<h1>Hi there</h1>
+        {<Heading2></Heading2>}
+		{Heading2()}
+	</>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render([heading1,heading2]);
+root.render(<Heading1 />);
 
 console.log("hi everyone again");
